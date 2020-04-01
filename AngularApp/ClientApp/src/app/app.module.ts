4e30274e989e376738359b9298321075a8fd7089
Develollo <lorenzo.caliciotti@gmail.com>
2020-04-01 15:45:49 +0200

    OK

--- AngularApp/ClientApp/src/app/app.module.ts
+++ AngularApp/ClientApp/src/app/app.module.ts
@@ -4,7 +4,7 @@ import { FormsModule } from '@angular/forms';
 import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
 import { RouterModule } from '@angular/router';
 import { AppComponent } from './app.component';
-import { HomeComponent } from './home/home.component';
+import { HomeComponent } from './feature/home/home.component';
 
 
 @NgModule({
